8bb26f7397178576c8723164bd5695b400b093c0
Peter Kehl <peter.kehl@gmail.com>
2018-12-18 19:08:27 -0800

    parse-lines

--- src/aoc18/core.clj
+++ src/aoc18/core.clj
@@ -7,3 +7,10 @@
     (= 0 (str/index-of c \.)) :open-ground
     (= 0 (str/index-of c \|)) :trees
     (= 0 (str/index-of c \#)) :lumberard))
+
+(defn parse-lines [text]
+  (let [lines (clojure.string/split text #"\n")]
+    (mapv #(seq %) lines)
+    ))
+(assert (= (parse-lines ".|\n#.")
+           [[\.\|][\#\.]]))
