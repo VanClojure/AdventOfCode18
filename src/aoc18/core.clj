154437d92185f9ac60fe300f3a0121ef6f35fe4d
Justin Belanger <justindbelanger@outlook.com>
2018-12-18 20:05:26 -0800

    Added how-many-neighbours and helper fns

--- src/aoc18/core.clj
+++ src/aoc18/core.clj
@@ -14,3 +14,37 @@
     ))
 (assert (= (parse-lines ".|\n#.")
            [[\.\|][\#\.]]))
+
+(def test-board1 [[:. :# :. :# :|]
+                  [:. :. :. :# :|]
+                  [:. :# :| :| :.]])
+
+(defn neighbour-coordinates [[x y]]
+  (for [i (range (dec x) (+ x 2))
+        j (range (dec y) (+ y 2))
+        :when (or (not= i x)
+                  (not= j y))
+        ]
+    [i j]
+    ))
+#_(assert (= (neighbour-coordinates [1 1])
+           [[0 1] [0 2] [0 3] [1 1] [1 3] [2 1] [2 2] [2 3]]))
+
+(defn how-many-neighbours [board [x y :as pair]]
+  (let [coordinates (neighbour-coordinates pair)
+        neighbours (map (partial get-in board) coordinates)]
+    (->> neighbours
+        (filter (fn [c] (not (nil? c))))
+        #_(reduce (fn [counts neighbour]
+                  (update counts neighbours inc))
+                  {})
+        (group-by identity)
+        (map (fn [[k v]] {k (count v)}))
+        (apply merge {:. 0 :| 0 :# 0}))))
+(assert (= (how-many-neighbours [[:. :# :.]
+                                 [:. :. :.]
+                                 [:. :# :|]]
+                                [1 1])
+           {:. 5, :| 1, :# 2}))
+
+#_{:. 1 :| 2 :# 0}
